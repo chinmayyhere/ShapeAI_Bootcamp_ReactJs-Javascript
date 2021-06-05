b87997c442121af57934c3114953402b9d5310f3
chinmayyhere <85386887+chinmayyhere@users.noreply.github.com>
2021-06-05 12:51:32 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Header() {
+  return <header>My fav of The Weeknd's </header>;
+}
+
+export default Header;
---
+++ Note.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div classname="note">
+      <h1>JavaScript and React.js</h1>
+      <p>
+        <ul>
+          <li>heartless</li>
+
+          <li>save your tears</li>
+
+          <li>the hills</li>
+
+          <li>call out my name</li>
+
+          <li>starboy</li>
+        </ul>
+      </p>
+    </div>
+  );
+}
+
+export default Note;
